d749dc5a296e88055a5d1bb8c7bf70e1c8abbbde
Idaho Edokpayi <idaho_edokpayi@ios.doi.gov>
2019-11-04 17:30:11 -0500

    renamed files

--- src/ResponseForm/formatDate.ts
+++ src/ResponseForm/formatDate.ts
@@ -1,6 +1,6 @@
 export function formatDate(date: Date): string {
     const year = date.getFullYear().toString();
-    const month = date.getMonth().toString().padStart(2, "0");
+    const month = (date.getMonth() +1).toString().padStart(2, "0");
     const day = date.getDate().toString().padStart(2, "0");
     const dash = "-";
     return year + dash + month + dash + day;
--- src/ResponseForm/threatLevelOptionChecked.tsx
+++
@@ -1,4 +0,0 @@
-import { ThreatLevelEnum } from '../types';
-export function threatLevelOptionChecked(threatLevel: ThreatLevelEnum, value?: ThreatLevelEnum): boolean {
-    return value === threatLevel;
-}
