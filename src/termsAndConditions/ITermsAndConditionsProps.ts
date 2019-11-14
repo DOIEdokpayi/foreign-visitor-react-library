e5d0685b6dc36b62387647bc71528d9ade930c8d
Idaho Edokpayi <idaho_edokpayi@ios.doi.gov>
2019-11-14 14:30:21 -0500

    got rid of redirect in terms and conditions

--- src/termsAndConditions/ITermsAndConditionsProps.ts
+++ src/termsAndConditions/ITermsAndConditionsProps.ts
@@ -2,6 +2,5 @@ import { ITermsAndConditionsFormSubmissionAction } from "../types";
 export interface ITermsAndConditionsProps {
   IconUrl: string;
   TermsAccepted: boolean;
-  Redirect: string;
   SubmitActionFunc: ITermsAndConditionsFormSubmissionAction;
 }
